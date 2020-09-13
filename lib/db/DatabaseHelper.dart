2c2aa3d2a0c00e6efbc161404ee55f75440c0e9b
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-09-13 10:26:26 +0100

    updated database code

--- lib/db/DatabaseHelper.dart
+++ lib/db/DatabaseHelper.dart
@@ -1,8 +1,10 @@
+import 'package:notes/models/Note.dart';
 import 'package:notes/utils/customviews/constants.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   static DatabaseHelper _databaseHelper;
+  static Database _database;
 
   factory DatabaseHelper() {
     if (_databaseHelper == null)
@@ -18,9 +20,41 @@ class DatabaseHelper {
 
   DatabaseHelper._createInstance();
 
+  Future<Database> get database async {
+    if (_database == null) _database = await initializeDatabase();
+    return _database;
+  }
+
   _createDb(Database db, int newVersion) async {
     await db.execute('CREATE TABLE ${Constants.TABLE_NAME}(${Constants.ID} ' +
         'INTEGER PRIMARY KEY AUTOINCREMENT, ${Constants.DESCRIPTION} TEXT,' +
         '${Constants.PRIORITY} INTEGER, ${Constants.DATE} TEXT )');
+  }
+
+  Future<List<Map<String, dynamic>>> getAllNote() async {
+    Database db = await this.database;
+    var result = db.rawQuery(
+        'SELECT * FROM ${Constants.TABLE_NAME} order by ${Constants.PRIORITY} ASC');
+    return result;
+  }
+
+  Future<int> insertNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.insert(Constants.TABLE_NAME, note.toMap());
+    return result;
+  }
+
+  Future<int> updateNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.update(Constants.TABLE_NAME, note.toMap(),
+        where: '${Constants.PRIORITY} =? ', whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> getCount() async {
+    Database db = await this.database;
+    var x = await db.rawQuery('SELECT (*) FROM ${Constants.TABLE_NAME}');
+    int result = Sqflite.firstIntValue(x);
+    return result;
   }
 }
